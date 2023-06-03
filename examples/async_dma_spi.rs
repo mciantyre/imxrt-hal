99774c7c140a932cc49065dd668b06910e6fe0c5
Ian McIntyre <ianpmcintyre@gmail.com>
2023-06-03 18:05:22 -0400

    Check generated code when dropping a Pinned future
    Assuming that cargo-binutils is available, building this example like
    
      cargo objdump --example=async_dma_spi --features=board/teensy4,board/spi --target=thumbv7em-none-eabihf -- -D
    
    and inspecting the generated code reveals that `drop()` is called on the
    DMA `Write` object in `main()`.

--- examples/async_dma_spi.rs
+++ examples/async_dma_spi.rs
@@ -13,23 +13,23 @@ fn main() -> ! {
     let mut chan_a = dma[board::BOARD_DMA_A_INDEX].take().unwrap();
     chan_a.set_disable_on_completion(true);
 
-    let mut chan_b = dma[board::BOARD_DMA_B_INDEX].take().unwrap();
-    chan_b.set_disable_on_completion(true);
+    let data: [u32; 5] = [1, 2, 3, 4, 5];
+    {
+        let dma_transfer = spi.dma_write(&mut chan_a, &data).unwrap();
+        cassette::pin_mut!(dma_transfer);
+        is_pinned_write_object(&dma_transfer); // <-- NEW: demonstrate shadowing and pinning.
+        let mut cm = cassette::Cassette::new(dma_transfer);
+        cm.poll_on();
+        core::mem::forget(cm);
+    }
 
-    let task = async {
-        let mut elem: u32 = 0;
-        loop {
-            let expected = [elem; 8];
-            let mut actual = [elem; 8];
-            spi.dma_full_duplex(&mut chan_a, &mut chan_b, &mut actual)
-                .unwrap()
-                .await
-                .unwrap();
-            assert_eq!(actual, expected);
-            elem = elem.wrapping_add(1);
-        }
-    };
-    pin_utils::pin_mut!(task);
-    board::blocking::run(task);
+    drop(data);
     unreachable!();
 }
+
+use core::pin::Pin;
+
+/// Shorthand for the `Write` object...
+type Write<'a> = imxrt_hal::dma::peripheral::Write<'a, board::Spi, u32>;
+
+fn is_pinned_write_object(_: &Pin<&mut Write<'_>>) {}
