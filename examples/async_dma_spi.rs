5a57e0c3d0d5594e317495e33a1e953f6c4111e0
Ian McIntyre <ianpmcintyre@gmail.com>
2023-06-11 08:08:57 -0400

    Demonstrate unsoundness in DMA design
    Heap pinning defeats the considerations I made when proposing the DMA
    design. By moving the DMA future to the heap then forgetting the future,
    we satisfy the drop guarantee while still corruping stack memory. The
    initial design always assumed stack-allocated-and-pinned DMA objects,
    which are very hard to forget while keeping the memory (footprint of the
    DMA object) intact.

--- examples/async_dma_spi.rs
+++ examples/async_dma_spi.rs
@@ -6,30 +6,64 @@
 #![no_std]
 #![no_main]
 
+extern crate alloc;
+use alloc::boxed::Box;
+
+use core::{
+    future::Future,
+    task::{Context, Waker},
+};
+
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+fn init_heap() {
+    let heap_start = imxrt_rt::heap_start();
+    let heap_end = imxrt_rt::heap_end() as usize;
+    let heap_size = heap_end - heap_start as usize;
+    unsafe {
+        ALLOCATOR.lock().init(heap_start.cast(), heap_size);
+    }
+}
+
+fn nop_waker() -> Waker {
+    use core::task::{RawWaker, RawWakerVTable};
+    const VTABLE: RawWakerVTable = RawWakerVTable::new(|_| RAW_WAKER, |_| {}, |_| {}, |_| {});
+
+    const RAW_WAKER: RawWaker = RawWaker::new(core::ptr::null(), &VTABLE);
+    // Safety: raw waker meets documented requirements.
+    unsafe { Waker::from_raw(RAW_WAKER) }
+}
+
 #[imxrt_rt::entry]
 fn main() -> ! {
+    init_heap();
     let (board::Common { mut dma, .. }, board::Specifics { mut spi, .. }) = board::new();
 
     let mut chan_a = dma[board::BOARD_DMA_A_INDEX].take().unwrap();
     chan_a.set_disable_on_completion(true);
 
-    let mut chan_b = dma[board::BOARD_DMA_B_INDEX].take().unwrap();
-    chan_b.set_disable_on_completion(true);
+    let buffer = &mut [0u32; 32];
+    // dma_obj borrows stack-allocated buffer.
+    let dma_obj = spi.dma_read(&mut chan_a, buffer).unwrap();
+    // dma_obj moved and pinned to heap.
+    let mut dma_obj = Box::pin(dma_obj);
 
-    let task = async {
-        let mut elem: u32 = 0;
-        loop {
-            let expected = [elem; 8];
-            let mut actual = [elem; 8];
-            spi.dma_full_duplex(&mut chan_a, &mut chan_b, &mut actual)
-                .unwrap()
-                .await
-                .unwrap();
-            assert_eq!(actual, expected);
-            elem = elem.wrapping_add(1);
-        }
-    };
-    pin_utils::pin_mut!(task);
-    board::blocking::run(task);
+    // Start the DMA transfer into stack memory.
+    let _ = dma_obj
+        .as_mut()
+        .poll(&mut Context::from_waker(&nop_waker()));
+
+    // Lose access to the DMA transfer, preventing drop.
+    // This statisfies the Pin drop guarantee while corrupting
+    // stack memory.
+    //
+    // Initial design always assumes stack-allocated DMA objects,
+    // which we can't forget without violating the drop guarantee.
+    // That assumption doesn't hold for heap pinning.
+    core::mem::forget(dma_obj);
+
     unreachable!();
 }
