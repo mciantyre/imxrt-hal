7c3591591e1f3d28965bacd5bd941a98f4bb4360
Ian McIntyre <ianpmcintyre@gmail.com>
2024-05-01 08:39:54 -0400

    Use a buffer of u16s for the source
    The LPSPI naturally supports a u32 word size. But since we always want a
    16 bit frame, we can use 16 bit LPSPI words as our DMA source without
    issue.

--- examples/async_dma_spi.rs
+++ examples/async_dma_spi.rs
@@ -9,9 +9,12 @@
 use hal::dma::channel::Channel;
 use imxrt_hal as hal;
 
-/// We want the DMA engine to perform 32 bit reads from the buffer, but 16-bit writes
+/// We want the DMA engine to perform 16 bit reads from the buffer and 16 bit writes
 /// to the LPSPI transmit data register. This manually sets up that transaction.
-async fn do_custom_dma(channel: &mut Channel, source: &[u32], destination: &mut board::Spi) {
+///
+/// We need to write this manually because today's LPSPI implementation only supports
+/// 32-bit DMA within a single transaction.
+async fn do_custom_dma(channel: &mut Channel, source: &[u16], destination: &mut board::Spi) {
     use hal::dma::{
         channel::{self, Configuration},
         peripheral::Destination,
@@ -21,8 +24,8 @@ async fn do_custom_dma(channel: &mut Channel, source: &[u32], destination: &mut 
     // instead on every invocation of this function.
     channel.set_channel_configuration(Configuration::enable(destination.destination_signal()));
     unsafe {
-        // Treat the buffer as a collection of u32s. This tells the DMA engine
-        // to perform 32 bit reads.
+        // Treat the buffer as a collection of u16s. This tells the DMA engine
+        // to perform 16 bit reads.
         channel::set_source_linear_buffer(channel, source);
 
         // Treat the hardware port as a u16. This tells the DMA engine to perform
@@ -31,9 +34,9 @@ async fn do_custom_dma(channel: &mut Channel, source: &[u32], destination: &mut 
         let destination_address: *const u16 = destination_address.cast();
         channel::set_destination_hardware(channel, destination_address);
 
-        // Move 4 bytes (32 bits) on every service request. By the above configurations,
-        // the DMA engine understands it should perform one read, then two writes.
-        channel.set_minor_loop_bytes(4);
+        // Move 2 bytes (16 bits) on every service request. By the above configurations,
+        // the DMA engine understands it should perform one read, then one write.
+        channel.set_minor_loop_bytes(core::mem::size_of::<u16>() as u32);
         // Move every element in the buffer.
         channel.set_transfer_iterations(source.len() as u16);
 
@@ -76,9 +79,9 @@ fn main() -> ! {
         spi.enqueue_transaction(&trans);
 
         loop {
-            let mut outgoing = [0u32; 32];
+            let mut outgoing = [0u16; 256];
             for (idx, elem) in outgoing.iter_mut().enumerate() {
-                *elem = ((0xFFFFu32 - idx as u32) << 16) | idx as u32;
+                *elem = idx as u16;
             }
             do_custom_dma(&mut chan_a, &outgoing, &mut spi).await;
             delay();
